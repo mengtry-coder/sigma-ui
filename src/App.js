dd431b392928515c034b4a6beb9685c4213ea0f3
Mengtry <measmengtry191@gmail.com>
2020-11-07 23:26:25 +0700

    First Setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+    <Router>
+      <Navbar></Navbar>
+    </Router>
+    </>
+  );
+}
+
+export default App;
